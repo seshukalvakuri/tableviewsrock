efaded325721e2dce23422385a81b54f982617ba
Kalvakuri,Lakshmi Seshu <s533707@ch140034m.local>
2019-02-19 10:52:41 -0600

    first view

--- TableViews Rock/FirstViewController.swift
+++ TableViews Rock/FirstViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var cities = ["Paris", "London", "Adelaide", "New York", "Hyderabad"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return cities.count
+        }
+            else {
+                return -1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "city")!
+        cell.textLabel?.text = cities[indexPath.row]
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
